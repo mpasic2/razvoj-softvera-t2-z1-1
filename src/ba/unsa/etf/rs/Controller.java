d13dc537c9876bb8b3f00ce2242db5f556004c1a
Amina Alijagic <aalijagic2@etf.unsa.ba>
2020-03-19 15:56:42 +0100

    Funkcionalnosti i dizajn Zadatka 1

--- src/ba/unsa/etf/rs/Controller.java
+++ src/ba/unsa/etf/rs/Controller.java
@@ -1,4 +1,11 @@
 package ba.unsa.etf.rs;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class Controller {
+    public Label helloWorld;
+    public void zdravo(ActionEvent actionEvent) {
+        helloWorld.setText("HelloWorld");
+    }
 }
